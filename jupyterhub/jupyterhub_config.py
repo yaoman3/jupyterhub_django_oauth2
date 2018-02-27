6831ce6369978690e4b1213b0bbe17ffe90b8014
Yaoman Li <yli6@lenovo.com>
2018-02-27 15:19:34 +0800

    Change to local docker version

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -4,7 +4,11 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 from oauthenticator.generic import GenericOAuthenticator
 
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.JupyterHub.authenticator_class = GenericOAuthenticator
+
+from jupyter_client.localinterfaces import public_ips
+c.JupyterHub.hub_ip = public_ips()[0]
 
 client_id = open(os.environ['OAUTH_CLIENT_ID_PATH']).read().strip()
 client_secret = open(os.environ['OAUTH_CLIENT_SECRET_PATH']).read().strip()
@@ -13,4 +17,4 @@ c.GenericOAuthenticator.client_secret = client_secret
 logger.debug("ID: " + client_id)
 logger.debug("SECRET: " + client_secret)
 
-c.JupyterHub.ip = '0.0.0.0'
+# c.JupyterHub.ip = '0.0.0.0'
